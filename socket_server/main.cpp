e84f0907db0d28817e68e24e73da0032a40f30ea
myswirl <myswirl@163.com>
2011-11-24 15:29:50 +0000

    1.socket:用simple socket库实现socket客户端和服务端。

---
+++ socket_server/main.cpp
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------------
+// ECHO SERVER CODE
+//------------------------------------------------------------------------------
+#include "PassiveSocket.h"       // Include header for active socket object definition
+
+#define MAX_PACKET 4096 
+
+int main(int argc, char **argv)
+{
+    CPassiveSocket socket;
+    CActiveSocket *pClient = NULL;
+	
+    //--------------------------------------------------------------------------
+    // Initialize our socket object 
+    //--------------------------------------------------------------------------
+    socket.Initialize();
+	
+    socket.Listen((const uint8 *)"127.0.0.1", 6789);
+	
+    while (true)
+    {
+        if ((pClient = socket.Accept()) != NULL)
+        {
+            //------------------------------------------------------------------
+            // Receive request from the client.
+            //------------------------------------------------------------------
+            if (pClient->Receive(MAX_PACKET))
+            {
+			
+                //--------------------------------------------------------------
+                // Send response to client and close connection to the client.
+                //--------------------------------------------------------------
+                pClient->Send((const uint8 *)pClient->GetData(), pClient->GetBytesReceived());
+                pClient->Close();
+            }
+			
+            delete pClient;
+        }
+    }
+	
+    //--------------------------------------------------------------------------
+    // Receive request from the client.
+    //--------------------------------------------------------------------------
+    socket.Close();
+	
+    return 1;
+}
