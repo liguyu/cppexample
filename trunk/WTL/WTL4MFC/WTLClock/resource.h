c0076282793180ac745e1813b9958f7ef51723f9
myswirl@163.com <myswirl@163.com@82ad174a-f0f8-11dd-aa46-671127399dce>
2011-12-28 08:14:06 +0000

    1.WTL/WTL4MFC/WTLClock:WTL时钟显示小程序，Part II WTL GUI Base Classes。

---
+++ trunk/WTL/WTL4MFC/WTLClock/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by WTLClock.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDC_START                       32772
+#define IDC_STOP                        32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
