a3caca6ff5e555c59d0b717338d5288c02bd6373
myswirl <myswirl@163.com>
2011-12-31 02:54:10 +0000

    1.WTL/LoadAging:bkwin基本框架界面。

--- WTL/LoadAging/LoadAging/MainDlg.cpp
+++ WTL/LoadAging/LoadAging/MainDlg.cpp
@@ -7,38 +7,64 @@
 
 #include "MainDlg.h"
 
-LRESULT CMainDlg::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+LRESULT CMainDlg::OnMaxWindow()
+{
+	if (WS_MAXIMIZE == (GetStyle() & WS_MAXIMIZE))
+	{		
+		SendMessage(WM_SYSCOMMAND, SC_RESTORE | HTCAPTION, 0);
+	}
+	else
+	{		
+		SendMessage(WM_SYSCOMMAND, SC_MAXIMIZE | HTCAPTION, 0);
+	}
+	return 0;
+}
+
+LRESULT CMainDlg::OnMinWindow()
+{
+	SendMessage(WM_SYSCOMMAND, SC_MINIMIZE, 0);
+	return 0;
+}
+
+void CMainDlg::OnSysCommand(UINT nID, CPoint point)
+{
+	SetMsgHandled(FALSE);
+
+	switch (nID & 0xFFF0)
+	{
+	case SC_CLOSE:
+		SetMsgHandled(TRUE);
+		//OnClose();
+		EndDialog(IDOK);
+		return;
+		break;
+	case SC_RESTORE:
+		{
+			DWORD dwStyle = GetStyle();
+			if (WS_MINIMIZE == (dwStyle & WS_MINIMIZE))
+				break;
+
+			if (WS_MAXIMIZE == (dwStyle & WS_MAXIMIZE))
+			{
+				SetItemAttribute(IDC_BTN_MAX, "skin", "dlg_btn_max");
+				break;
+			}
+		}
+	case SC_MAXIMIZE:
+		SetItemAttribute(IDC_BTN_MAX, "skin", "dlg_btn_normal");
+		break;
+	default:
+		break;
+	}
+}
+
+LRESULT CMainDlg::OnInitDialog(HWND, LPARAM)
 {
 	// center the dialog on the screen
 	CenterWindow();
 
-	// set icons
-	HICON hIcon = (HICON)::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDR_MAINFRAME), 
-		IMAGE_ICON, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON), LR_DEFAULTCOLOR);
-	SetIcon(hIcon, TRUE);
-	HICON hIconSmall = (HICON)::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDR_MAINFRAME), 
-		IMAGE_ICON, ::GetSystemMetrics(SM_CXSMICON), ::GetSystemMetrics(SM_CYSMICON), LR_DEFAULTCOLOR);
-	SetIcon(hIconSmall, FALSE);
 
 	return TRUE;
 }
 
-LRESULT CMainDlg::OnAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-{
-	CSimpleDialog<IDD_ABOUTBOX, FALSE> dlg;
-	dlg.DoModal();
-	return 0;
-}
 
-LRESULT CMainDlg::OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-{
-	// TODO: Add validation code 
-	EndDialog(wID);
-	return 0;
-}
-
-LRESULT CMainDlg::OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-{
-	EndDialog(wID);
-	return 0;
-}
