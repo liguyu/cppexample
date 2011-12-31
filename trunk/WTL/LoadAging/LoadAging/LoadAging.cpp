b0c947ea0c7876ae81ae4ec337cc80d3c0e47f3f
myswirl@163.com <myswirl@163.com@82ad174a-f0f8-11dd-aa46-671127399dce>
2011-12-31 01:00:27 +0000

    1.WTL/LoadAging:负载老化系统，金山界面库实现的，KUI+WTL。

---
+++ trunk/WTL/LoadAging/LoadAging/LoadAging.cpp
@@ -0,0 +1,39 @@
+// LoadAging.cpp : main source file for LoadAging.exe
+//
+
+#include "stdafx.h"
+
+#include "resource.h"
+
+#include "MainDlg.h"
+
+CAppModule _Module;
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR /*lpstrCmdLine*/, int /*nCmdShow*/)
+{
+	HRESULT hRes = ::CoInitialize(NULL);		//初始化COM
+// If you are running on NT 4.0 or higher you can use the following call instead to 
+// make the EXE free threaded. This means that calls come in on a random RPC thread.
+//	HRESULT hRes = ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	ATLASSERT(SUCCEEDED(hRes));
+
+	// this resolves ATL window thunking problem when Microsoft Layer for Unicode (MSLU) is used
+	::DefWindowProc(NULL, 0, 0, 0L);
+
+	AtlInitCommonControls(ICC_BAR_CLASSES);		// add flags to support other controls
+
+	hRes = _Module.Init(NULL, hInstance);		//全局对象_Module被初始化
+	ATLASSERT(SUCCEEDED(hRes));
+
+	int nRet = 0;
+	// BLOCK: Run application, 将CMainDlg变量放在一个区块中是很重要的
+	{
+		CMainDlg dlgMain;
+		nRet = dlgMain.DoModal();
+	}
+
+	_Module.Term();
+	::CoUninitialize();
+
+	return nRet;
+}
